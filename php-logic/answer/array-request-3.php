24010fbeddbd49380177cb99536885a4534d6097
Rahman <ainurahman03@gmail.com>
2021-01-07 10:24:45 +0700

    menyelesaikan array-request-3.php

--- php-logic/answer/array-request-3.php
+++ php-logic/answer/array-request-3.php
@@ -15,7 +15,7 @@ $input = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Array-request-2</title>
+    <title>Array-request-3</title>
 </head>
 
 <body>
