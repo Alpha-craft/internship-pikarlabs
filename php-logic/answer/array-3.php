2b0313f39cea201bb5a6f79781ba71dd26c6efac
Alpha_Craft <57023140+Alpha-craft@users.noreply.github.com>
2021-01-09 14:26:09 +0700

    Update array-3.php

--- php-logic/answer/array-3.php
+++ php-logic/answer/array-3.php
@@ -6,7 +6,7 @@ foreach($input as $key => $value){
     //periksa satu persatu value dari array terlebih dahulu
     // $key berisi index dan $varible $value berisi value array tersebut
     if ($cari == $value ){
-        //cek menggunakan if apakah $cari sesuai dengan $cari jika sesuai maka echo indexnya yaitu $value
+        //cek menggunakan if apakah $cari ada dalam $value jika sesuai maka echo indexnya yaitu $key
         echo "index ke $key";
         
     }
