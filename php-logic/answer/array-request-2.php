15a12d1bc2f9e62d1fb34fa13c40a80de9d3e9cd
Rahman <ainurahman03@gmail.com>
2021-01-15 21:22:06 +0700

    rewrite array-request-2.php

--- php-logic/answer/array-request-2.php
+++ php-logic/answer/array-request-2.php
@@ -1,96 +1,37 @@
 <?php
-$input = [
-    [ 'id' => 122, 'nama' => 'rama', 'asal' => 'surabaya' ],
-    [ 'id' => 227, 'nama' => 'renaldy', 'asal' => 'surabaya' ],
-    [ 'id' => 423, 'nama' => 'fakhri', 'asal' => 'surabaya' ],
-];
-
-
-
+include "component/data.php";
+include "component/func.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Array-request-2</title>
+    <title>Test array request</title>
 </head>
-
 <body>
     <div class="container">
-        <form action="" method="get">
-            <label for="search">Cari ID</label>
-            <input type="text" name="id" id="search" autofocus required>
-            <div class="btn-group">
-                <button type="submit"  class="btn btn-success">Cari</button>
-                <a class="btn btn-danger" href="array-request-2.php">
-                    Reset
-                </a>
-            </div>
-        </form>
-        <?php if(isset($_GET['id'])): ?>
-            <table style="display:none;" border='1'>
-                <tr>
-                    <th>id</th>
-                    <th>nama</th>
-                    <th>asal</th>
-                    <th>link</th>
-                </tr>
-                <?php foreach ($input as $value) :?>
-                    <tr>
-                        <td><?= $value['id'] ?></td>
-                        <td><?= $value['nama'] ?></td>
-                        <td><?= $value['asal'] ?></td>
-                        <td><a href="array-request-2.php?id=<?= $value['id'] ?>">profil</a></td>
-                    </tr>
-                <?php endforeach ?>
-            </table>
-        <?php else: ?>
-            <div class="table">
-                <table class="table-hover" border='1'>
-                    <tr>
-                        <th>id</th>
-                        <th>nama</th>
-                        <th>asal</th>
-                        <th>link</th>
-                    </tr>
-                    <?php foreach ($input as $value) :?>
-                        <tr>
-                            <td><?= $value['id'] ?></td>
-                            <td><?= $value['nama'] ?></td>
-                            <td><?= $value['asal'] ?></td>
-                            <td><a href="array-request-2.php?id=<?= $value['id'] ?>">profil</a></td>
-                        </tr>
-                    <?php endforeach ?>
-                </table>
-            </div>
-        <?php endif ?>
-        <?php if(isset($_GET['id'])): ?>
-            <div class="table">
-                <table class="table-hover" border='1' >
-                    <tr>
-                        <th>id</th>
-                        <th>nama</th>
-                        <th>asal</th>
-                        <th>link</th>
-                    </tr>
-                    <?php foreach ($input as $result) :?>
-                        <?php if( strpos ($result['id'],$_GET['id'] ) !== false ): ?>
-                            <!-- memakai strpos() untuk mengecek apakah id berada dalam array['id'] -->
-                            <tr>
-                                <td><?= $result['id'] ?></td>
-                                <td><?= $result['nama'] ?></td>
-                                <td><?= $result['asal'] ?></td>
-                                <td><a href="array-request-2.php?id=<?= $result['id'] ?>">profil</a></td>
-                            </tr>
-                        <?php endif ?>
-                    <?php endforeach ?>
-                </table>
-            </div>
-        <?php endif ?>
+    <form action="" method="get">
+        <label for="search">Cari ID</label>
+        <input type="text" name="id" id="search" autofocus required  >
+        <div class="btn-group">
+            <button type="submit"  class="btn btn-success">Cari</button>
+            <a class="btn btn-danger" href="?">
+                Reset
+            </a>
+        </div>
+    </form>
+    <?php if(isset($_GET['id'])): ?>
+        <?php $get = $_GET['id'] ?>
+        <h4>Hasil yang ditemukan untuk  <?= "\" $get \"" ?> </h4>
+        <?php $find = getID($get) ?>
+        <?php search($input,$get) ?>
+    <?php else: ?>
+        <?php showTable($input) ?>
+            
+    <?php endif ?>
+        
     </div>
 </body>
-
 </html>
