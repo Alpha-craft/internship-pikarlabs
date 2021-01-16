145ef87712881916d95786eae85b84626826b05a
Rahman <ainurahman03@gmail.com>
2021-01-16 17:33:13 +0700

    menyelesaikan segitiga C

--- php-logic/answer/looping-conditional-1.php
+++ php-logic/answer/looping-conditional-1.php
@@ -1,6 +1,7 @@
 <?php
 $input = 5;
 
+if($input % 2)
 echo "Segitiga A , input=$input ";
 echo "<br>";
 for($i = 1;$i <= $input; $i++){
@@ -17,5 +18,15 @@ for($i = 1;$i <=$input;$i++){
     }
     echo "<br>";
 }
-
+echo "Segitiga C,input = $input";
+echo "<br>";
+for($i=1; $i <= $input; $i++){
+    for($j=$input; $j >= $i; $j--){
+        echo " &nbsp";
+    }
+    for($k=1; $k <= $i; $k++){
+        echo "*";
+    }
+    echo "<br> ";
+}
 ?>
