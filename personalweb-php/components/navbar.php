9454017f395c8a67554e668e4b1b504ddca6965e
Rahman <ainurahman03@gmail.com>
2021-01-18 21:12:30 +0700

    membuat navbar

--- personalweb-php/components/navbar.php
+++ personalweb-php/components/navbar.php
@@ -1,6 +1,7 @@
-<div class="bg-green-400 h-12 flex ">
+<div class="bg-green-500 font-sans h-14 text-2xl" >My Website</div>
+<div class="bg-green-400 h-8 flex ">
     <?php foreach ($nav_item as $nav) :?>
-        <div id="<?= $nav?>" class="flex-1 flex-grow flex-shrink  hover:bg-green-500 cursor-pointer text-center md:text-xl inline-block align-middle"><?= $nav ?></div>
+        <a href="#<?= $nav ?>" id="<?= $nav?>" class="flex-1 flex-grow flex-shrink  hover:bg-green-500  cursor-pointer text-center md:text-xl hover:text-gray-50"><?= $nav ?></a>
     <?php endforeach ?>
     
 </div>
