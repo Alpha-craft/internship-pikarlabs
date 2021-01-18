b04825a7b0ca9d91e551a2116ea08f12f20a9e15
Rahman <ainurahman03@gmail.com>
2021-01-18 20:21:06 +0700

    test

--- personalweb-php/components/navbar.php
+++ personalweb-php/components/navbar.php
@@ -1,5 +1,6 @@
-<div class="bg-green-400 h-24 grid grid-cols-3 ">
+<div class="bg-green-400 h-12 flex ">
     <?php foreach ($nav_item as $nav) :?>
-        <div id="<?= $nav?>" class="col-span-1 hover:bg-green-500 text-center"><?= $nav ?></div>
+        <div id="<?= $nav?>" class="flex-1 flex-grow flex-shrink  hover:bg-green-500 cursor-pointer text-center md:text-xl inline-block align-middle"><?= $nav ?></div>
     <?php endforeach ?>
+    
 </div>
