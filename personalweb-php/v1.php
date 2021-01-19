4c4ee04e82cb1ec9a5e928541f52601ca24d0a77
Rahman <ainurahman03@gmail.com>
2021-01-19 13:31:47 +0700

    Menambahkan border dan bg-color pada bio

--- personalweb-php/v1.php
+++ personalweb-php/v1.php
@@ -11,10 +11,12 @@ include "components/header.php";
 <div class="container mx-auto ">
 
     <!-- Bio Start -->
-    <div class="inline-block ">
-        <img class="rounded-full w-44 border-2 border-white mx-auto" src="assets/profile.jpg" alt="Profil-Img" id="profil">
+    <div class="relative inline-block ">
+        <div class="my-8 bg-gray-100 py-6 border-t-2 border-l-2 border-r-2 border-solid border-gray-700">
+            <img class="rounded-full w-44 border-2 border-white mx-auto " src="assets/profile.jpg" alt="Profil-Img" id="profil">
+            <h4 class="text-2xl font-sans text-center" >Ainurahman</h4>
+        </div>
         <div id="Profil" class="py-8 capitalize border-solid  border-b-2  border-gray-700 bg-gray-100 inline-block float-center">
-            <h4 class="text-2xl font-sans" >Ainurahman</h4>
             <p class="font-mono" ><span class="mx-4"></span> <?= $bio ?> </p>
         </div>
     </div>
