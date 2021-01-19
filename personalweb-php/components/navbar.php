260fdc0eab2cab9e940e7d8fad59a3a27aedae08
Rahman <ainurahman03@gmail.com>
2021-01-19 17:20:58 +0700

    menambahkan efek shadow dan mengganti warna navbar

--- personalweb-php/components/navbar.php
+++ personalweb-php/components/navbar.php
@@ -1,11 +1,13 @@
-<div class="bg-green-500 font-sans h-14  md:text-2xl " >
-    <div class="inline-flex">
-        <img class="rounded-full w-12 border-2 border-white " src="assets/profile.jpg" alt="Profil-Img" id="profil">
+<div class="bg-white font-sans h-14 sticky top-0 shadow-md md:text-2xl " >
+    <div class="flex">
+        <img class="rounded-full w-12 border-2 border-current " src="assets/profile.jpg" alt="Profil-Img" id="profil">
         <p class="text-4xl italic">Personal Web</p>
     </div>
-</div>
-<div class="bg-green-400 h-8 flex ">
-    <?php foreach ($nav_item as $nav) :?>
-        <a href="#<?= $nav ?>" id="<?= $nav?>" class="flex-1 flex-grow flex-shrink  hover:bg-green-500  cursor-pointer text-center md:text-xl hover:text-gray-50"><?= $nav ?></a>
-    <?php endforeach ?>
+    
+    <div class="bg-white h-8 flex ">
+        <?php foreach ($nav_item as $nav) :?>
+            <a href="#<?= $nav ?>" id="<?= $nav?>" class="transition duration-300 ease-out rounded flex-1 flex-grow flex-shrink  cursor-pointer text-center md:text-xl hover:bg-black hover:text-gray-100"><?= $nav ?></a>
+        <?php endforeach ?>
+    </div>
+
 </div>
--- personalweb-php/v1.php
+++ personalweb-php/v1.php
@@ -6,24 +6,30 @@ include "components/header.php";
 <?php include "components/navbar.php" ?>
 <!-- Navbar end -->
 
-    
-<!-- Container start -->
-<div class="container mx-auto ">
+<div class="bg-gray-100">
+    <!-- Container start -->
+    <div class="container mx-auto pt-12 ">
 
-    <!-- Bio Start -->
-    <div class="relative inline-block ">
-        <div class="my-8 bg-gray-100 py-6 border-t-2 border-l-2 border-r-2 border-solid border-gray-700">
-            <img class="rounded-full w-44 border-2 border-white mx-auto " src="assets/profile.jpg" alt="Profil-Img" id="profil">
-            <h4 class="text-2xl font-sans text-center" >Ainurahman</h4>
+        <!-- Bio Start -->
+        <div class="relative inline-block ">
+            <div class="transition duration-500 ease-in-out my-8 bg-gray-100 hover:bg-white rounded-md hover:shadow-xl py-6 ">
+                <img class="rounded-full w-44 border-2 border-gray-200 mx-auto " src="assets/profile.jpg" alt="Profil-Img" id="profil">
+                <h4 class="text-2xl font-sans text-center" >Ainurahman</h4>
+            </div>
+            <div id="Profil" class="transition duration-500 ease-in-out py-8 rounded-md hover:shadow-lg capitalize hover:bg-white bg-gray-100 ">
+                <p class="font-mono" ><span class="mx-4"></span> <?= $bio ?> </p>
+            </div>
         </div>
-        <div id="Profil" class="py-8 capitalize border-solid  border-b-2  border-gray-700 bg-gray-100 inline-block float-center">
-            <p class="font-mono" ><span class="mx-4"></span> <?= $bio ?> </p>
+        <!-- Bio end -->
+
+        <!-- Skill Start-->
+        <div class="my-10 bg-white " >
+
         </div>
+        <!-- Skill end -->
+
     </div>
-    <!-- Bio end -->
-
+    <!-- Container end -->
 </div>
-<!-- Container end -->
-
 
 <?php include "components/footer.php" ?>
