ceab489f910dafea52e4d060ca309563962b4901
Rahman <ainurahman03@gmail.com>
2021-01-28 14:49:27 +0700

    memperbaiki footer

--- personalweb-php/components/footer.php
+++ personalweb-php/components/footer.php
@@ -1,4 +1,6 @@
-<div class="bg-gray-700 w-auto h-auto m-0 p-2" >
+<div class="bg-gray-700 w-auto h-auto m-0 p-2 text-gray-50 text-center" >
+<p class=" text-xl  ">&copy 2021 Rahman</p>
+<p class="capitalize text-xl " >Website Development and design by Rahman</p>
 
 </div>
 
