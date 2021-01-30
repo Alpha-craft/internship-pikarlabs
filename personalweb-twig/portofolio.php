0d89e4407c03fc753bbcb919652cbd2b9a131d1f
Rahman <ainurahman03@gmail.com>
2021-01-30 23:34:04 +0700

    mengganti beberapa variabel yang menyimpan nama file untuk gambarnya

--- personalweb-twig/portofolio.php
+++ personalweb-twig/portofolio.php
@@ -8,11 +8,13 @@ $cek = false;
 if( isset($_GET['id'] ) ){
   @$id = $_GET['id'];
   @$cek =  cekId($_GET['id'],$portofolio);
-  @$image = $portofolio[$id]['imgs'] ;
+  @$galeri = $portofolio[$id]['imgs'] ;
   @$judul = $portofolio[$id]['judul'];
   @$desc = $portofolio[$id]['desc'];
   @$tahun = $portofolio[$id]['tahun'];
   @$main_image = $portofolio[$id]['img'];
+  @$image = getImg($main_image,$galeri);
+  @$link = $portofolio[$id]['link'];
 }
 else{
   header("location:v2.php");
@@ -27,6 +29,8 @@ echo $twig->render('portofolio.twig',[
   'desc' => $desc,
   'tahun' => $tahun,
   'main_image' => $main_image,
+  'galeri' => $galeri,
+  'link' => $link,
 ]);
 
 ?>
